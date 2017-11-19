d6564311594a4582ac4a988cf38290c1a74609d3
Ryan Coyne <ryan@clearcodex.com>
2017-11-18 23:12:55 -0500

    Release 1.0.1.

---
+++ Example/Pods/Target Support Files/CCXGoogleNearbyPlaces/CCXGoogleNearbyPlaces-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CCXGoogleNearbyPlaces : NSObject
+@end
+@implementation PodsDummy_CCXGoogleNearbyPlaces
+@end
---
+++ Example/Pods/Target Support Files/Pods-CCXGoogleNearbyPlaces_Tests/Pods-CCXGoogleNearbyPlaces_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CCXGoogleNearbyPlaces_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CCXGoogleNearbyPlaces_Tests
+@end
