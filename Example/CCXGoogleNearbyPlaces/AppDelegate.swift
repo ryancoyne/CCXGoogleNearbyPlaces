8fb6a350e87cbb3cd97a751e58280f0a1c864f5b
Ryan Coyne <ryan@clearcodex.com>
2018-12-24 14:18:23 -0500

    Updating to swift 4.2

--- Example/CCXGoogleNearbyPlaces/AppDelegate.swift
+++ Example/CCXGoogleNearbyPlaces/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         CCXGoogleSDK.places.apiKey = "your api key"
         let coord = CLLocationCoordinate2D(latitude: 38.928544, longitude: -77.0240332)
